4cd35dee59d9cdfc9b3f40465a3729facfde7760
Aneesh0631 <101163524+Aneesh0631@users.noreply.github.com>
2024-03-11 19:54:48 +0530

    Completed
    Final commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+email : any = "aneesh06251@gmail.com"
 }
